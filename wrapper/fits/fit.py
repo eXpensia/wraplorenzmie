6e981f286c0c83fe5568faa5c742cc2a39d40a0b
eXpensia <maximelavaud@yahoo.fr>
2019-11-05 23:34:11 +0100

    write fit class and black formating.

--- wrapper/fits/fit.py
+++ wrapper/fits/fit.py
@@ -1,23 +1,73 @@
 import pylorenzmie
+from pylorenzmie.theory import Instrument
+from pylorenzmie.theory.Feature import Feature
+from tqdm import tqdm
 
-class fitter(object):
-    ''' All the wrap aroud the pylorenzmie toolbox
+
+class fitting(object):
+    """ All the wrap aroud the pylorenzmie toolbox
     available at the grier lab github
-    https://github.com/davidgrier/pylorenzmie '''
+    https://github.com/davidgrier/pylorenzmie """
 
-    def __init__(image,wavelength,magnification,n_m=1.33,
-                    dark_count=0,background=1):
+    def __init__(
+        image, wavelength, magnification, n_m=1.33, dark_count=0, background=1
+    ):
         self.image = image
         self.shape = image.shape
-        self.instrument = Instrument.Instrument(wavelength=wavelength
-                                    magnification=magnification, #microns/pixel
-                                    n_m=n_m,
-                                    dark_count=dark_count,
-                                    background=background)
+        self.instrument = Instrument.Instrument(
+            wavelength=wavelength,
+            magnification=magnification,  # um/pixel
+            n_m=n_m,
+            dark_count=dark_count,
+            background=background,
+        )
 
+    def make_guess(self, a_p, n_p, z, fit_nr=True):
+        """ Add the guess to the object to fit more nicely, z is to be put in
+        µm for easier usage, it will be put in pixels in the actual guess dict"""
+        if fit_nr == True:
+            vary = [True] * 5
+            vary.extend([False] * 5)
+            self.vary = vary
+        else:
+            vary = [True] * 3
+            self.vary = vary
+            vary.extend([False] * 7)
+        self.shape = cropped_norm.shape
+        self.guesses = {
+            "a_p": a_p,  # um
+            "n_p": n_p,
+            "r_p": [self.shape[0] // 2, self.shape[1] // 2, z / self.magnification],
+        }
 
-    def make_guess(self,a_p,n_p,z):
-        self.guesses =
-        guesses = {'a_p': .92, #um
-                   'n_p': 1.41,
-                   'r_p': [shape[0] // 2, shape[1] // 2, z]} #pixels
+    def fit_single(self, image):
+        """Fit an hologram with the given guesses"""
+
+        fitter = Feature(**self.guesses)
+
+        fitter.FitSettings.parameters.vary = self.vary
+        fitter.model.instrument = instrument
+        fitter.model.coordinates = Instrument.coordinates(shape)
+        fitter.data = (image).reshape(image.size)
+        return fitter.optimize()
+
+    def update_guess(self, x, y, z):
+        self.guesses["r_p"] = [x, y, z]
+
+    def fit_video(self, vid, background, savefile, n_start=1, n_end=None):
+        """Fit a full movie by just using the guesses of the first image
+        the guesses for the next image will take the precedent ones"""
+        if n_end == None:
+            print("Computing the length of the video")
+            n_end = vid.get_length
+            print("length of video = {}".format(self.number))
+
+        image = vid.get_image
+
+        for i in tqdm(range(n_start, n_end)):
+            if i > n_start:
+                image = normalize(vid.get_next_image(), background)
+
+            result = self.fit_single(image)
+            # to do save the result to the JSON file
+            self.update_guess(**result.final[0:4])
--- wrapper/utilities/utilities.py
+++ wrapper/utilities/utilities.py
@@ -9,8 +9,7 @@ import trackpy as tp
 
 
 class video_reader(object):
-
-    def __init__(self, filename, number = None,background = None, codecs = None):
+    def __init__(self, filename, number=None, background=None, codecs=None):
         self.filename = filename
         self.codecs = codecs
         if codecs == None:
@@ -23,43 +22,51 @@ class video_reader(object):
     def close(self):
         self.vid.close()
 
-    def get_image(self,n):
-        ''' Get the image n of the movie '''
-        return np.array(self.vid.get_data(n)[:,:,1])
+    def get_image(self, n):
+        """ Get the image n of the movie """
+        return np.array(self.vid.get_data(n)[:, :, 1])
 
-    def get_filtered_image(self,n,sigma):
-        ''' Get the image n of the movie and apply a gaussian filter '''
-        return gaussian_filter(self.vid.get_data(n)[:,:,1], sigma = sigma)
+    def get_next_image(self):
+        return np.array(self.vid.get_next_data()[:, :, 1])
 
-    def get_background(self,n):
-        ''' Compute the background over n images spread out on all the movie'''
+    def get_filtered_image(self, n, sigma):
+        """ Get the image n of the movie and apply a gaussian filter """
+        return gaussian_filter(self.vid.get_data(n)[:, :, 1], sigma=sigma)
+
+    def get_background(self, n):
+        """ Compute the background over n images spread out on all the movie"""
         if self.number == None:
-            print("needs to compute length of the movie.")
+            print("needs to compute length of the video.")
             self.get_length()
-            print("length of movie = {}".format(self.number))
+            print("length of video = {}".format(self.number))
 
         image = self.get_image(1)
-        size = (n,image.shape[0],image.shape[1])
-        buf = np.empty(size,dtype=np.uint8)
+        size = (n, image.shape[0], image.shape[1])
+        buf = np.empty(size, dtype=np.uint8)
         with tqdm(total=n) as pbar:
-            for i,toc in enumerate(np.arange(0,self.number, self.number // (n-1))):
-                buf[i,:,:] = self.get_image(toc)
+            for i, toc in enumerate(np.arange(0, self.number, self.number // (n - 1))):
+                buf[i, :, :] = self.get_image(toc)
                 pbar.update(1)
 
         self.background = np.median(buf, axis=0)
         return self.background
 
     def get_length(self):
-        self.number = self.vid.count_frames()
+        """Read the number of frame of vid, can be long with some format as mp4
+        so we don't read it again if we already got it"""
+        if self.number == None:
+            self.number = self.vid.count_frames()
+            return self.number
+        else:
+            return self.number
 
 
-
-
-def center_find(image,n_fringes=28, trackpy_params={'diameter': 30,
-                                    'minmass': None,
-                                    'topn': None,
-                                    'engine': 'numba'}):
-    '''
+def center_find(
+    image,
+    n_fringes=28,
+    trackpy_params={"diameter": 30, "minmass": None, "topn": None, "engine": "numba"},
+):
+    """
     Courtesy of https://github.com/davidgrier/pylorenzmie/
     Example wrapper that uses orientational alignment transform (OAT)
     and trackpy.locate to return features in an image.
@@ -77,46 +84,62 @@ def center_find(image,n_fringes=28, trackpy_params={'diameter': 30,
                   detection. (xc, yc) is the center of the particle, and
                   (w, h) = (201, 201) by default.
         circ: circle_transform of original image for plotting.
-    '''
-    circ = ct.circletransform(image, theory='orientTrans')
+    """
+    circ = ct.circletransform(image, theory="orientTrans")
     circ = circ / np.amax(circ)
     circ = h5.TagArray(circ, frame_no=None)
     features = tp.locate(circ, **trackpy_params)
-    features['w'] = 201
-    features['h'] = 201
-    features = np.array(features[['x', 'y', 'w', 'h']])
+    features["w"] = 201
+    features["h"] = 201
+    features = np.array(features[["x", "y", "w", "h"]])
 
     # Find extent of detected features and change default bounding box.
     for idx, feature in enumerate(features):
         xc = feature[0]
         yc = feature[1]
-        s = localize.feature_extent(norm,
-                                    (xc, yc),
-                                    nfringes = nfringes)
+        s = localize.feature_extent(norm, (xc, yc), nfringes=nfringes)
         features[idx][2] = s
         features[idx][3] = s
 
     return features, circ
 
-def plot_bounding(image,features):
+
+def plot_bounding(image, features):
     fig, ax = plt.subplots()
-    ax.imshow(norm, cmap='gray')
+    ax.imshow(norm, cmap="gray")
     for feature in features:
         x, y, w, h = feature
-        test_rect = Rectangle(xy=(x - w/2, y - h/2), width=w,
-                            height=h, fill=False, linewidth=3, edgecolor='r')
+        test_rect = Rectangle(
+            xy=(x - w / 2, y - h / 2),
+            width=w,
+            height=h,
+            fill=False,
+            linewidth=3,
+            edgecolor="r",
+        )
         ax.add_patch(test_rect)
+    plt.plot()
+
+
+def normalize(image, background, dark_count=None):
+    if dark_count == None:
+        return (image - dark_count) / (background - dark_count)
+    else:
+        return image / backgroud
 
 
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
     import time
+
     t = time.time()
-    vid = video_reader("/media/maxime/Maxtor/09102019/film75fps1_15k/Basler_acA1920-155um__22392621__20191009_143652597.mp4")
+    vid = video_reader(
+        "/media/maxime/Maxtor/09102019/film75fps1_15k/Basler_acA1920-155um__22392621__20191009_143652597.mp4"
+    )
     vid.number = 156604
     bg = vid.get_background(100)
     t = time.time() - t
     vid.close()
     print(t)
-    plt.imshow(bg,cmap='gray')
+    plt.imshow(bg, cmap="gray")
     plt.show()
